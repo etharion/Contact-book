d50634765f5adc3272fd687c8f86daaa98a7e37a
etharion <warlordcp@gmail.com>
2015-11-30 10:33:28 +0100

    exceptions added

---
+++ contactbook.exceptions/src/exceptions/InvalidInformationException.java
@@ -0,0 +1,10 @@
+package exception;
+
+/**
+ * Default-implementeret exception som benyttes når en information er ugyldig.
+ * 
+ */
+@SuppressWarnings("serial")
+public class InvalidInformationException extends Exception {
+
+}
